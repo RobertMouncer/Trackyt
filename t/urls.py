43d6e10f86e5917f3f0ee63efdecf77671c221aa
Robert Mouncer <44447819+RobertMouncer@users.noreply.github.com>
2020-10-14 12:45:26 +0100

    changed admin url

--- t/urls.py
+++ t/urls.py
@@ -4,7 +4,7 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='homepage/index.html'), name='home'),
-    path('admin/', admin.site.urls),
+    path('hiddenadminpagethatnooneshouldeverfind/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('accounts/', include('accounts.urls'))
 ]
