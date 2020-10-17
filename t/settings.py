e49d1ab8a11900189231c38d0fc3e8902c8fa2c4
Robert Mouncer <44447819+RobertMouncer@users.noreply.github.com>
2020-10-17 10:00:22 +0100

    added ssl settings

--- t/settings.py
+++ t/settings.py
@@ -125,4 +125,11 @@ STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 
+if not DEBUG:
+
+    SECURE_SSL_REDIRECT = True
+
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    
+
 django_heroku.settings(locals())
